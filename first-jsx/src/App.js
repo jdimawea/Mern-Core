81de21ac784d53cbaa5d6a2e44f1f92170caa2ec
Jonathan Dima <dimawea@gmail.com>
2022-03-14 16:31:27 -0600

    Commit

---
+++ first-jsx/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FirstJsx from './components/FirstJsx'
+
+function App() {
+  return (
+    <div className="App">
+        < FirstJsx />
+    </div>
+  );
+}
+export default App;
+
---
+++ first-jsx/src/components/FirstJsx.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class FirstJsx extends Component {
+    render() {
+        return (
+        <div>
+            <h1>Hello Dojo!</h1>
+            <h2>Things I need to do:</h2>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a Marathon</li>
+            <li>Feed the dogs</li>
+        </div>
+        )
+    }
+}
+
+export default FirstJsx
