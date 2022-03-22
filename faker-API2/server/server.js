4c910f76278ad35b570628b1befd163730b47112
Jonathan Dima <dimawea@gmail.com>
2022-03-22 14:31:04 -0600

    commit

---
+++ faker-API2/server/server.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const app = express()
+const port = 8000
+const { faker } = require('@faker-js/faker');
+
+
+class User {
+    constructor() {
+        this.id = faker.datatype.number();
+        this.firstName = faker.name.firstName();
+        this.lastName = faker.name.lastName();
+        this.phoneNumber = faker.phone.phoneNumber();
+        this.email = faker.internet.email();
+        this.password = faker.internet.password();
+    }
+}
+
+class Company {
+    constructor() {
+        this.id = faker.datatype.number();
+        this.name = faker.company.companyName();
+        this.address = {
+            street: faker.address.streetAddress(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+    }
+}
+
+
+app.get("/api/users/new", (req, res) => {
+    res.json(new User);
+})
+
+app.get("/api/companies/new", (req, res) => {
+    res.json(new Company);
+})
+
+app.get("/api/user/company", (req, res) => {
+    res.json({ user: new User(), company: new Company() });
+})
+
+app.get("/api", (req, res) => {
+    res.json("Welcome to the Faker API")
+})
+
+
+app.listen(8000, () => console.log(`Listening on port: 8000`))
